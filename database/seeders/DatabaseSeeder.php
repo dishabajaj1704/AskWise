45c27c38777bc36d80881cbac488be2a89a7e68b
Disha Bajaj <dishabajaj1508@gmail.com>
2023-06-01 20:53:17 +0530

    Created Dummy data for answers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Answer;
 use App\Models\Question;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -22,15 +23,29 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('abcd1234')
         ]);
 
+        // \App\Models\User::factory(10)->create()->each(function ($user) {
+        //     for ($i = 0; $i < random_int(5, 10); $i++) {
+        //         $user->questions()->create(
+        //             Question::factory()
+        //                 ->make()
+        //                 ->toArray()
+        //         );
+        //     }
+        // });
+
+
         \App\Models\User::factory(10)->create()->each(function ($user) {
-            for ($i = 0; $i < random_int(5, 10); $i++) {
-                $user->questions()->create(
-                    Question::factory()
-                        ->make()
-                        ->toArray()
+            $user->questions()->saveMany(
+                Question::factory(random_int(5, 15))->make()
+            )->each(function ($question) {
+                $question->answers()->saveMany(
+                    Answer::factory(random_int(2, 10))->make()
                 );
-            }
+            });
+
+
         });
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
