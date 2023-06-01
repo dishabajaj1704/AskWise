dcd70f832e245dc692a22e42ee2eab46ababce75
Disha Bajaj <dishabajaj1508@gmail.com>
2023-06-01 16:13:05 +0530

    Created own user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Question;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Auth\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        User::create([
+            'name' => 'Disha Bajaj',
+            'email' => 'disha@gmail.com',
+            'password' => Hash::make('abcd1234')
+        ]);
+
         \App\Models\User::factory(10)->create()->each(function ($user) {
             for ($i = 0; $i < random_int(5, 10); $i++) {
                 $user->questions()->create(
