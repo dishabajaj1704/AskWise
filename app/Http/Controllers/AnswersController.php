eae6a6d285733908996854f291a7c6f528605fc5
Disha Bajaj <dishabajaj1508@gmail.com>
2023-06-03 20:23:15 +0530

    Created markAsBest function

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateAnswerRequest;
+use App\Http\Requests\MarkAsBestRequest;
 use App\Http\Requests\UpdateAnswerRequest;
 use App\Models\Answer;
 use App\Models\Question;
@@ -40,9 +41,21 @@ class AnswersController extends Controller
         return redirect($question->url);
     }
 
-    public function edit(Question $question,Answer $answer){
-        $this->authorize('update',$answer);
-        return view('answers.edit',compact(['question','answer']));
+    public function edit(Question $question, Answer $answer)
+    {
+        $this->authorize('update', $answer);
+        return view('answers.edit', compact(['question', 'answer']));
+
+    }
+
+    public function markAsBest(MarkAsBestRequest $request, Question $question, Answer $answer)
+    {
+        $this->authorize('markAsBest', $question);
+        if ($answer->question->id !== $question->id) {
+            abort(403); //Inspect
+        }
+        $question->markAsBest(($answer));
+        return redirect()->back();
 
     }
 
