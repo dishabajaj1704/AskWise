594d27cca41152bf7a6904631051bfdfe73bbccc
Disha Bajaj <dishabajaj1508@gmail.com>
2023-06-04 20:39:29 +0530

    Added VotesController route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AnswersController;
 use App\Http\Controllers\FavoritesController;
 use App\Http\Controllers\QuestionsController;
+use App\Http\Controllers\VotesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,3 +31,5 @@ Route::resource('questions.answers', AnswersController::class)->except(['create'
 Route::put('/questions/{question}/answers/{answer}/mark-as-best', [AnswersController::class, 'markAsBest'])->name('questions.answers.markAsBest');
 Route::post('/questions/{question}/mark-as-fav', [FavoritesController::class, 'store'])->name('questions.mark-as-fav');
 Route::delete('/questions/{question}/mark-as-unfav', [FavoritesController::class, 'destroy'])->name('questions.mark-as-unfav');
+
+Route::post('/questions/{question}/vote/{vote}', [VotesController::class, 'voteQuestion'])->name('questions.vote');
