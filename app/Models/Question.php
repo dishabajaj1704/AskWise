dfd7a818095f4dd983378e48422f6fb6c8346689
Disha Bajaj <dishabajaj1508@gmail.com>
2023-06-04 20:25:47 +0530

    Added logic for votes to be updated or create

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -76,4 +76,25 @@ class Question extends Model
     {
         return $this->morphToMany(User::class, 'vote')->withTimestamps();
     }
+
+    public function vote(int $vote)
+    {
+        $this->votes()->attach(auth()->id(), ['vote' => $vote]);
+        if ($vote < 0) {
+            $this->decrement('votes_count');
+        } else {
+            $this->increment('votes_count');
+        }
+    }
+
+    public function updateVote(int $vote)
+    {
+        $this->votes()->updateExistingPivot(auth()->id(), ['vote' => $vote]);
+        $this->votes()->attach(auth()->id(), ['vote' => $vote]);
+        if ($vote < 0) {
+            $this->decrement('votes_count', 2);
+        } else {
+            $this->increment('votes_count', 2);
+        }
+    }
 }
